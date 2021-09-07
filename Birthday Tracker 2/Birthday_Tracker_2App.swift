74541bfd747cc950a789c778c28f4fb8085935b3
Oliver Foggin <oliverfoggin@me.com>
2021-09-07 10:53:49 +0100

    Initial Commit

---
+++ Birthday Tracker 2/Birthday_Tracker_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_Tracker_2App.swift
+//  Birthday Tracker 2
+//
+//  Created by Foggin, Oliver (Developer) on 07/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Birthday_Tracker_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

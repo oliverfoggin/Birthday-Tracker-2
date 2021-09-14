8f62973bdebe315df05a4506f56b5260cf82ce9d
Oliver Foggin <oliverfoggin@me.com>
2021-09-15 00:55:59 +0100

    Improve next occurence calculation

--- Common/Extensions/Date.swift
+++ Common/Extensions/Date.swift
@@ -3,11 +3,11 @@ import Foundation
 public extension Date {
   func nextOccurence(from now: Date, using calendar: Calendar) -> Date {
     calendar.nextDate(
-      after: self,
-      matching: calendar.dateComponents([.day, .month], from: now),
-      matchingPolicy: Calendar.MatchingPolicy.nextTimePreservingSmallerComponents,
-      repeatedTimePolicy: Calendar.RepeatedTimePolicy.first,
-      direction: Calendar.SearchDirection.forward
+      after: now,
+      matching: calendar.dateComponents([.day, .month], from: self),
+      matchingPolicy: .previousTimePreservingSmallerComponents,
+      repeatedTimePolicy: .first,
+      direction: .forward
     )!
   }
 }

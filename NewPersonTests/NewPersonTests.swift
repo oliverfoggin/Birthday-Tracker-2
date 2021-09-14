eb4be157fd11f9d0efef91a845e7e8257dde5dc6
Oliver Foggin <oliverfoggin@me.com>
2021-09-15 00:45:48 +0100

    Updated tests a bit

--- NewPersonTests/NewPersonTests.swift
+++ NewPersonTests/NewPersonTests.swift
@@ -13,13 +13,16 @@ import ComposableArchitecture
 class NewPersonTests: XCTestCase {
   func test() throws {
     let testStore = TestStore(
-      initialState: NewPersonState.init(dob: Date(timeIntervalSince1970: 0)),
+      initialState: .init(dob: Date(timeIntervalSince1970: 0)),
       reducer: newPersonReducer,
       environment: .init()
     )
     
     testStore.send(.binding(.set(\.$name, "Oliver"))) {
       $0.name = "Oliver"
+    }
+    testStore.send(.binding(.set(\.$dob, Date(timeIntervalSince1970: 100)))) {
+      $0.dob = Date(timeIntervalSince1970: 100)
     }
   }
 }
